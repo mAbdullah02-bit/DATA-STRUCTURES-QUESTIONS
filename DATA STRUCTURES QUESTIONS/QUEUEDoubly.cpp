122beb22abbdf8333a42d1ea0fae2f0e1a1edf2e
abdullah12 <f230581@cfd.nu.edu.pk>
2024-10-01 20:52:33 +0500

    Updated these questions

--- DATA STRUCTURES QUESTIONS/QUEUEDoubly.cpp
+++ DATA STRUCTURES QUESTIONS/QUEUEDoubly.cpp
@@ -1,8 +1,7 @@
 //#include<iostream>
 //using namespace std;
 //
-//
-//class deque{
+//class deque {
 //    int* dequeArr;
 //    int maxSize, rear, front;
 //
@@ -10,132 +9,141 @@
 //    deque(int s) {
 //        maxSize = s;
 //        dequeArr = new int[s];
-//        for (int i = 0; i < maxSize; i++)
-//        {
+//        for (int i = 0; i < maxSize; i++) {
 //            dequeArr[i] = 0;
 //        }
 //        rear = front = -1;
 //    }
+//
 //    bool isEmpty() {
-//        if (rear == -1 && front == -1)return true;
-//        else return false;
+//        return (rear == -1 && front == -1);
 //    }
-//    bool isFull(){
-//        if ((rear+1)%maxSize==front)
-//        {
-//            cout << "Queue IS FULL\n"; return true;
-//        }
-//        return false;
+//
+//    bool isFull() {
+//        return ((rear + 1) % maxSize == front);
 //    }
 //
 //    void insertFront(int value) {
-//        if (!isFull())
-//        {
-//
-//        
-//        if (isEmpty())
-//        {
-//            front = maxSize-1;
+//        if (isFull()) {
+//            cout << "Deque is full.\n";
+//            return;
 //        }
-//        else front--;
+//
+//        if (isEmpty()) {
+//            front = rear = 0;
+//        }
+//        else {
+//            front = (front - 1 + maxSize) % maxSize;
+//        }
 //
 //        dequeArr[front] = value;
-//    
 //    }
-//        else cout << "LIST IS FULL>>>>\n";
-//    }
+//
 //    void insertBack(int value) {
-//        if (!isFull())
-//        {
-//
-//            if (isEmpty())
-//            {
-//                rear = 0;
-//            }
-//            else rear++;
-//
-//            dequeArr[rear] = value;
+//        if (isFull()) {
+//            cout << "Deque is full.\n";
+//            return;
 //        }
-//        else cout << "LIST IS FULL>>>>\n";
-//    
+//
+//        if (isEmpty()) {
+//            front = rear = 0;
+//        }
+//        else {
+//            rear = (rear + 1) % maxSize;
+//        }
+//
+//        dequeArr[rear] = value;
 //    }
+//
 //    int removeFront() {
-//        if (isEmpty())
-//        {
-//            cout << "List is empty..\n";
-//        }
-//        else
-//        {
-//            int value = dequeArr[front];
-//            dequeArr[front] = 0;
-//            front++;
-//            return value;
+//        if (isEmpty()) {
+//            cout << "Deque is empty.\n";
+//            return -1;
 //        }
 //
+//        int value = dequeArr[front];
+//        dequeArr[front] = 0;
+//
+//        if (front == rear) { 
+//            front = rear = -1;
+//        }
+//        else {
+//            front = (front + 1) % maxSize;
+//        }
+//
+//        return value;
 //    }
+//
 //    int removeBack() {
-//        if (isEmpty())
-//        {
-//            cout << "List is empty..\n";
+//        if (isEmpty()) {
+//            cout << "Deque is empty.\n";
+//            return -1;
 //        }
-//        else
-//        {
-//            int value = dequeArr[rear];
-//            dequeArr[rear] = 0;
-//            rear--;
-//            return value;
+//
+//        int value = dequeArr[rear];
+//        dequeArr[rear] = 0;
+//
+//        if (front == rear) { 
+//            front = rear = -1;
 //        }
+//        else {
+//            rear = (rear - 1 + maxSize) % maxSize;
+//        }
+//
+//        return value;
 //    }
+//
 //    void display() {
-//    
-//        for (int i = 0; i < 10; i++)
-//        {
-//            cout<<dequeArr[i] << "  ";
+//        if (isEmpty()) {
+//            cout << "Deque is empty.\n";
+//            return;
 //        }
-//      
+//
+//        int i = front;
+//        while (true) {
+//            cout << dequeArr[i] << " ";
+//            if (i == rear)
+//                break;
+//            i = (i + 1) % maxSize;
+//        }
 //        cout << endl;
-//    
 //    }
+//
 //    ~deque() {
-//        delete [] dequeArr;
+//        delete[] dequeArr;
 //    }
 //};
 //
-//int main(){
-//
+//int main() {
 //    deque q(10);
+//
 //    q.insertFront(1);
 //    q.display();
+//
 //    q.insertFront(2);
 //    q.display();
+//
 //    q.insertBack(5);
 //    q.display();
+//
 //    q.insertBack(6);
 //    q.display();
+//
 //    q.insertFront(3);
 //    q.display();
+//
 //    q.insertFront(4);
 //    q.display();
+//
 //    q.insertBack(7);
 //    q.display();
-//    q.insertBack(8);
-//    
-//    q.display();
-//    q.display();
-//    q.insertBack(7);
-//    q.display();
+//
 //    q.insertBack(8);
 //    q.display();
-//    q.insertBack(7);
-//    q.display();
-//    q.insertBack(8);
-//    q.display();
+//
 //    q.removeFront();
 //    q.display();
-//    q.removeFront();
-//    q.display();
-//    q.removeBack();
-//    q.display();
+//
 //    q.removeBack();
 //    q.display();
 //
