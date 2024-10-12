a54a18ad19320e8c4c92fbcb20988cd59f3b891b
mAbdullah02-bit <abbi212711@gmail.com>
2024-10-12 11:57:22 +0500

    added questions

---
+++ DATA STRUCTURES QUESTIONS/sumofdigits-recursion.cpp
@@ -0,0 +1,25 @@
+//#include<iostream>
+//using namespace std;
+//
+//int sumofdigits(int n,int &sum) {
+//	
+//	if (n==0)
+//	{
+//		return sum;
+//	}
+//	
+//
+//	sum += n % 10;
+//	return sumofdigits(n /= 10,sum);
+//
+//}
+//
+//int main() {
+//	int sum = 0;
+//	cout << sumofdigits(123,sum);
+//
+//
+//
+//	return 0;
+//}
+// 
