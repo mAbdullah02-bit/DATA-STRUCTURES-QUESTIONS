70f2a5bbfdea85358d7d500a791839468888fe8e
mAbdullah02-bit <abbi212711@gmail.com>
2024-11-18 16:18:13 +0500

    ADDED BFS TECHNIQUES

---
+++ LAB-11/LAB-11/MAMtask2.cpp
@@ -0,0 +1,116 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+const int maxvertices = 10;
+
+class Graph {
+public:
+	string vertices[maxvertices];
+	int adjmatrix[maxvertices][maxvertices];
+	int noofvertices;
+	string color[maxvertices];
+	Graph() {
+		noofvertices = 0; for (int i = 0; i < maxvertices; i++)
+		{
+			for (int j = 0; j < maxvertices; j++)
+			{
+				adjmatrix[i][j] = 0;
+			}
+		}
+	}
+
+	void addvertices(string s) {
+		vertices[noofvertices] = s;
+		for (int i = 0; i < noofvertices; i++)
+		{
+			adjmatrix[noofvertices][i] = 0;
+			adjmatrix[i][noofvertices] = 0;
+		}
+		noofvertices++;
+
+	}
+	void addEdge(string from, string to) {
+
+		int fromIndex = findVertexIndex(from);
+		int toIndex = findVertexIndex(to);
+
+		if (fromIndex == -1 || toIndex == -1) {
+			cout << "One or both vertices not found.\n";
+			return;
+		}
+		adjmatrix[fromIndex][toIndex] = 1;
+	}
+	int	findVertexIndex(string s) {
+		for (int i = 0; i < noofvertices; i++)
+		{
+			if (vertices[i] == s)return i;
+		}
+		return -1;
+	}
+	void display() {
+
+
+		cout << "   ";
+		for (int i = 0; i < noofvertices; i++)
+		{
+			cout << i << " ";
+		}
+		cout << endl;
+		for (int i = 0; i < noofvertices; i++) {
+			cout << i << "  ";
+			for (int j = 0; j < noofvertices; j++) {
+				cout << adjmatrix[i][j] << " ";
+
+
+			}
+			cout << "\n";
+		}
+	}
+
+
+	void BFS(string startvertex) {
+		queue<int> q;
+		for (int i = 0; i < noofvertices; i++)
+		{
+			color[i] = "white";
+		}
+		int startindex = findVertexIndex(startvertex);
+		q.push(startindex);
+		color[startindex] = "gray";
+		while (!q.empty())
+		{
+			int index = q.front();
+			for (int i = 0; i < noofvertices; i++)
+			{
+				if (color[i] == "white" && adjmatrix[index][i]==1) {
+					q.push(i);
+					color[i] = "gray";
+				}
+			}
+			q.pop();
+			color[index] = "black";
+			cout << vertices[index] << " ";
+		}
+	
+	}
+};
+int main() {
+
+
+	Graph g;
+	g.addvertices("A");
+	g.addvertices("B");
+	g.addvertices("C");
+	g.addvertices("D");
+	g.addEdge("A","B");
+	g.addEdge("B","C");
+	g.addEdge("A","C");
+	g.addEdge("C","D");
+	g.addEdge("B", "D");
+	g.addEdge("B", "D");
+	g.display();
+	cout << endl << endl;
+	cout << endl << endl;
+	g.BFS("A");
+	return 0;
+}
--- LAB-11/LAB-11/Task 3.cpp
+++ LAB-11/LAB-11/Task 3.cpp
@@ -1,116 +1,116 @@
-#include <iostream>
-#include <queue>
-using namespace std;
-
-template <typename T>
-class Graph {
-    T* vertices;
-    int** arr;
-    bool directional;
-    int vertexsize;
-    int vertexCount;
-
-public:
-    Graph(int n) {
-        vertexsize = n;
-        vertexCount = 0;
-        vertices = new T[vertexsize];
-        arr = new int* [vertexsize];
-        for (int i = 0; i < vertexsize; i++) {
-            arr[i] = new int[vertexsize];
-            for (int j = 0; j < vertexsize; j++) {
-                arr[i][j] = 0;
-            }
-        }
-        directional = false;
-    }
-
-    void addVertex(T vertex) {
-        if (vertexCount >= vertexsize) {
-            cout << "Limit reached....\n";
-            return;
-        }
-        vertices[vertexCount++] = vertex;
-    }
-
-    int findVertexIndex(T vertex) {
-        for (int i = 0; i < vertexCount; i++) {
-            if (vertices[i] == vertex) return i;
-        }
-        return -1;
-    }
-
-    void addEdge(T from, T to) {
-        int fromIndex = findVertexIndex(from);
-        int toIndex = findVertexIndex(to);
-
-        if (fromIndex == -1 || toIndex == -1) {
-            cout << "One or both vertices not found.\n";
-            return;
-        }
-
-        arr[fromIndex][toIndex] = 1;
-        if (!directional) {
-            arr[toIndex][fromIndex] = 1;
-        }
-
-        cout << "Added edge between " << from << " and " << to << endl;
-    }
-
-    void display() {
-       
-        queue<pair<T, T>> edgeQueue;
-
-        
-        cout << "  ";
-        for (int i = 0; i < vertexCount; i++) {
-            cout << vertices[i] << " ";
-        }
-        cout << "\n";
-
-        for (int i = 0; i < vertexCount; i++) {
-            cout << vertices[i] << " ";
-            for (int j = 0; j < vertexCount; j++) {
-                cout << arr[i][j] << " ";
-               
-                if (arr[i][j] == 1 && (directional || i < j)) {
-                    edgeQueue.push({ vertices[i], vertices[j] });
-                }
-            }
-            cout << "\n";
-        }
-
-   
-        cout << "\nEdges in the graph:\n";
-        while (!edgeQueue.empty()) {
-            auto edge = edgeQueue.front();
-            edgeQueue.pop();
-            cout << edge.first << " -> " << edge.second << endl;
-        }
-    }
-
-    ~Graph() {
-        for (int i = 0; i < vertexsize; i++) {
-            delete[] arr[i];
-        }
-        delete[] arr;
-        delete[] vertices;
-    }
-};
-
-int main() {
-    Graph<char> g(5);
-
-    g.addVertex('A');
-    g.addVertex('B');
-    g.addVertex('C');
-    g.addVertex('D');
-
-    g.addEdge('A', 'B');
-    g.addEdge('B', 'C');
-    g.addEdge('C', 'D');
-    g.addEdge('D', 'A');
-
-    g.display();
-    return 0;
-}
+//#include <iostream>
+//#include <queue>
+//using namespace std;
+//
+//template <typename T>
+//class Graph {
+//    T* vertices;
+//    int** arr;
+//    bool directional;
+//    int vertexsize;
+//    int vertexCount;
+//
+//public:
+//    Graph(int n) {
+//        vertexsize = n;
+//        vertexCount = 0;
+//        vertices = new T[vertexsize];
+//        arr = new int* [vertexsize];
+//        for (int i = 0; i < vertexsize; i++) {
+//            arr[i] = new int[vertexsize];
+//            for (int j = 0; j < vertexsize; j++) {
+//                arr[i][j] = 0;
+//            }
+//        }
+//        directional = false;
+//    }
+//
+//    void addVertex(T vertex) {
+//        if (vertexCount >= vertexsize) {
+//            cout << "Limit reached....\n";
+//            return;
+//        }
+//        vertices[vertexCount++] = vertex;
+//    }
+//
+//    int findVertexIndex(T vertex) {
+//        for (int i = 0; i < vertexCount; i++) {
+//            if (vertices[i] == vertex) return i;
+//        }
+//        return -1;
+//    }
+//
+//    void addEdge(T from, T to) {
+//        int fromIndex = findVertexIndex(from);
+//        int toIndex = findVertexIndex(to);
+//
+//        if (fromIndex == -1 || toIndex == -1) {
+//            cout << "One or both vertices not found.\n";
+//            return;
+//        }
+//
+//        arr[fromIndex][toIndex] = 1;
+//        if (!directional) {
+//            arr[toIndex][fromIndex] = 1;
+//        }
+//
+//        cout << "Added edge between " << from << " and " << to << endl;
+//    }
+//
+//    void display() {
+//       
+//        queue<pair<T, T>> edgeQueue;
+//
+//        
+//        cout << "  ";
+//        for (int i = 0; i < vertexCount; i++) {
+//            cout << vertices[i] << " ";
+//        }
+//        cout << "\n";
+//
+//        for (int i = 0; i < vertexCount; i++) {
+//            cout << vertices[i] << " ";
+//            for (int j = 0; j < vertexCount; j++) {
+//                cout << arr[i][j] << " ";
+//               
+//                if (arr[i][j] == 1 && (directional || i < j)) {
+//                    edgeQueue.push({ vertices[i], vertices[j] });
+//                }
+//            }
+//            cout << "\n";
+//        }
+//
+//   
+//        cout << "\nEdges in the graph:\n";
+//        while (!edgeQueue.empty()) {
+//            auto edge = edgeQueue.front();
+//            edgeQueue.pop();
+//            cout << edge.first << " -> " << edge.second << endl;
+//        }
+//    }
+//
+//    ~Graph() {
+//        for (int i = 0; i < vertexsize; i++) {
+//            delete[] arr[i];
+//        }
+//        delete[] arr;
+//        delete[] vertices;
+//    }
+//};
+//
+//int main() {
+//    Graph<char> g(5);
+//
+//    g.addVertex('A');
+//    g.addVertex('B');
+//    g.addVertex('C');
+//    g.addVertex('D');
+//
+//    g.addEdge('A', 'B');
+//    g.addEdge('B', 'C');
+//    g.addEdge('C', 'D');
+//    g.addEdge('D', 'A');
+//
+//    g.display();
+//    return 0;
+//}
